937ce7509a4eb819083e39e6d70b56613413bec9
rishabhpandey202 <101575373+rishabhpandey202@users.noreply.github.com>
2022-05-20 21:39:48 +0530

    Update navbar.js

--- student mange System/navbar.js
+++ student mange System/navbar.js
@@ -1,12 +1,16 @@
 function navbar (){
-  return  `<form action="" onsubmit="Description(event)">
-    <input type="text" name="" id="name" placeholder="name">
-    <input type="text" name="" id="course" placeholder="course">
-    <input type="text" name="" id="unit" placeholder="unit">
-    <input type="url" name="" id="image" placeholder="image">
-    <input type="text" name="" id="batch" placeholder="batch">
-    <input type="submit" />
-    </form>
+  return  `<form action="" id="form" onsubmit="Description(event)">
+  <input type="text" name="" id="name" placeholder="name" required>
+  <input type="text" name="" id="course" placeholder="course" required>
+  <input type="number" name="" id="unit" placeholder="unit" required>
+  <input type="url" name="" id="image" placeholder="image" required>
+  <select name="" id="batch">
+      <option value="web16" id="web16" >web16</option>
+      <option value="web17" id="web17">web17</option>
+  </select>
+  <!-- <input type="text" name="" id="batch" placeholder="batch"> -->
+  <input type="submit" />
+  </form>
     `
 }
 
